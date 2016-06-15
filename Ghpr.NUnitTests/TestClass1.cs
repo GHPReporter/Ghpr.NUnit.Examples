4aa7c66c69a3c61d99c8c6ac24f7efbe358fe453
Evgeniy Kosjakov <42_@mail.ru>
2016-06-15 13:19:19 +0300

    tests again

--- Ghpr.NUnitTests/TestClass1.cs
+++ Ghpr.NUnitTests/TestClass1.cs
@@ -1,6 +1,35 @@
-﻿namespace Ghpr.NUnitTests
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Ghpr.NUnitTests
 {
+    [TestFixture]
     public class TestClass1
     {
+        [TestCase("0", 0, "11111111-1111-1111-1111-111111111211")]
+        [TestCase("1", 1, "11111111-1111-1111-1111-111111111212")]
+        [TestCase("2", 2, "11111111-1111-1111-1111-111111111213", TestName = "Testing name attribute")]
+        public void ParamTestName1(string input, int expected, string guid)
+        {
+            Thread.Sleep(200);
+            Assert.AreEqual(input, expected.ToString("D"));
+        }
+
+        [TestCase("0", 1, "11111111-1111-1111-1111-111111111214", TestName = "param test 1")]
+        [TestCase("1", 1, "11111111-1111-1111-1111-111111111215", TestName = "param test 2")]
+        [TestCase("2", 1, "11111111-1111-1111-1111-111111111216", TestName = "param test 3")]
+        public void ParamTestName2(string input, int expected, string guid)
+        {
+            Assert.AreEqual(input, expected.ToString("D"));
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            Console.WriteLine("Testing log writing 1");
+            Console.WriteLine("Testing log writing 2");
+            throw new Exception("Some error occured!");
+        }
     }
 }
---
+++ Ghpr.NUnitTests/TestClass2.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Ghpr.NUnitTests
+{
+    [TestFixture, Ignore("Ignored test fixture")]
+    public class TestClass2
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 2);
+        }
+
+        [Test, Category("SuccessCategory")]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
