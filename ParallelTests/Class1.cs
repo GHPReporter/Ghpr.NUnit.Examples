92b3a339ab7e94fbe3386a27f0ac51f3db4edd3c
Evgeniy Kosjakov <42_@mail.ru>
2016-12-20 13:54:23 +0300

    Parallel tests

---
+++ ParallelTests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+namespace ParallelTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1() { Thread.Sleep(1000); }
+
+        [Test]
+        public void Test2() { Thread.Sleep(1000); }
+
+        [Test]
+        public void Test3() { Thread.Sleep(1000); }
+    }
+}
